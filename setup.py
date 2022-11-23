2935e80d16224904b548e935e188d97862618d9b
kcrampon <kevin.crampon@univ-reims.fr>
2022-11-23 10:55:10 +0100

    Change for HGCN into setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bgcn_4_pls',
+    name='hgcn_4_pls',
     version='1.1',
-    description='A BiPartite Graph Convolutional Neural Network to score a protein-ligand complex.',
+    description='A Heterogeneous Graph Convolutional Neural Network to score a protein-ligand complex.',
     author='Kevin Crampon',
     author_email='kevin.crampon@univ-reims.fr',
-    url='https://github.com/KevinCrp/BGCN_4_PLS',
-    packages=find_packages(include=["bgcn_4_pls",
-                                    "bgcn_4_pls.casf",
-                                    "bgcn_4_pls.layers",
-                                    "bgcn_4_pls.networks",
-                                    "bgcn_4_pls.utilities"]),
+    url='https://github.com/KevinCrp/HGCN_4_PLS',
+    packages=find_packages(include=["hgcn_4_pls",
+                                    "hgcn_4_pls.casf",
+                                    "hgcn_4_pls.layers",
+                                    "hgcn_4_pls.networks",
+                                    "hgcn_4_pls.utilities"]),
     install_requires=[],
 )
